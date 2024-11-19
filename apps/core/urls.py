9f9b37dcce3454a0330ccf46eace4d491272a2b3
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2024-11-19 10:06:12 -0300

    criacao pagina contato

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
+from .views import contato
 
 urlpatterns = [
-    path('', home, name='home')  
+    path('', home, name='home') ,
+    path('contato/', contato, name='contato' ),
 ]
